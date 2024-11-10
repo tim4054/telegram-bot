ff358d05784e7f70cbd4a8921f52972ce3c5c044
tim40 <tim4054@gmail.com>
2024-11-10 23:48:07 +0300

    '/start' command completed

---
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -0,0 +1,52 @@
+package pro.sky.telegrambot.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+
+
+import com.pengrad.telegrambot.request.SendMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class TelegramBotUpdatesListener implements UpdatesListener {
+
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            if (update.message() != null && update.message().text() != null) {
+                Long chatID = update.message().chat().id();
+                String text = update.message().text();
+                StringBuilder answer = new StringBuilder();
+
+                if (text.equals("/start")) {
+                    answer.append("Hello");
+                } else {
+                    answer.append("Unsupported command");
+                }
+                SendMessage sendMessage = new SendMessage(chatID, answer.toString());
+                telegramBot.execute(sendMessage);
+            } else {
+                logger.warn("Received update without text message: {}", update);
+            }
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
